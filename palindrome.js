ee60fbc648c1101427096172d9763e8d58ed255c
Your Name <you@example.com>
2022-12-08 09:04:50 +0530

    checked

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,10 @@
-\\check palindrome
+// check palindrome
 
 let str="racecar";
 let reverse="";
     for(let i=((str.length)-1);i>=0;i--)
     {
-        rev+=str[i];
+        reverse+=str[i];
     }
     if (str=reverse){
         console.log("Yes");
--- prime.js
+++ prime.js
@@ -1,14 +1,15 @@
 //Check prime
+
 let num=15;
 let check=0;
-    for (let i=1;i<=num;i++){
-        if (i%num==0){
+    for (let i=1;i<=num/1;i++){
+        if (num%i==0){
             check++;
         }
         
         
     }
-    if (check==1){
+    if (check==0){
         console.log("Yes");
     }
     else {
